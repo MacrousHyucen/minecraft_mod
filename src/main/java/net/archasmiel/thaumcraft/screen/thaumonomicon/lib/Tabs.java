15153819f3794557a3c9d45d1383694cd16f0d3a
Sergey Remez <s.remez-me23@lll.kpi.ua>
2022-08-24 00:47:48 +0300

    [v0.0.6.5] - builder for tab class - changes in thaumonomicon lib package - tabs rewritten with builder

--- src/main/java/net/archasmiel/thaumcraft/screen/thaumonomicon/lib/Tabs.java
+++ src/main/java/net/archasmiel/thaumcraft/screen/thaumonomicon/lib/Tabs.java
@@ -2,25 +2,42 @@ package net.archasmiel.thaumcraft.screen.thaumonomicon.lib;
 
 import net.archasmiel.thaumcraft.screen.thaumonomicon.parts.Tab;
 
-import static net.archasmiel.thaumcraft.screen.thaumonomicon.lib.GuiUtil.eldritch_back;
-import static net.archasmiel.thaumcraft.screen.thaumonomicon.lib.GuiUtil.normal_back;
-import static net.archasmiel.thaumcraft.screen.thaumonomicon.lib.Icons.*;
+import static net.archasmiel.thaumcraft.screen.thaumonomicon.lib.Textures.*;
 
 public class Tabs {
 
     public static final String TRANSLATE_PATH = "gui.thaumcraft.";
     public static final Integer TAB_SIZE = 26;
 
-    public static final Tab BASICS       =  new Tab("basics",       normal_back,    TAB_SIZE, RT_BASICS);
-    static {
-        BASICS.setState(true);
-    }
+    public static final Tab BASICS = new Tab.Builder()
+            .id("basics").name()
+            .icon(RT_BASICS).background(NORMAL_BACKGROUND)
+            .size(TAB_SIZE).active().build();
 
-    public static final Tab THAUMATURGY  =  new Tab("thaumaturgy",  normal_back,    TAB_SIZE, RT_THAUMATURGY);
-    public static final Tab ALCHEMY      =  new Tab("alchemy",      normal_back,    TAB_SIZE, RT_ALCHEMY);
-    public static final Tab ARTIFICE     =  new Tab("artifice",     normal_back,    TAB_SIZE, RT_ARTIFICE);
-    public static final Tab GOLEMANCY    =  new Tab("golemancy",    normal_back,    TAB_SIZE, RT_GOLEMANCY);
-    public static final Tab ELDRITCH     =  new Tab("eldritch",     eldritch_back,  TAB_SIZE, RT_ELDRITCH);
+    public static final Tab THAUMATURGY = new Tab.Builder()
+            .id("thaumaturgy").name()
+            .icon(RT_THAUMATURGY).background(NORMAL_BACKGROUND)
+            .size(TAB_SIZE).build();
+
+    public static final Tab ALCHEMY = new Tab.Builder()
+            .id("alchemy").name()
+            .icon(RT_ALCHEMY).background(NORMAL_BACKGROUND)
+            .size(TAB_SIZE).build();
+
+    public static final Tab ARTIFICE = new Tab.Builder()
+            .id("artifice").name()
+            .icon(RT_ARTIFICE).background(NORMAL_BACKGROUND)
+            .size(TAB_SIZE).build();
+
+    public static final Tab GOLEMANCY = new Tab.Builder()
+            .id("golemancy").name()
+            .icon(RT_GOLEMANCY).background(NORMAL_BACKGROUND)
+            .size(TAB_SIZE).build();
+
+    public static final Tab ELDRITCH = new Tab.Builder()
+            .id("eldritch").name()
+            .icon(RT_ELDRITCH).background(ELDRITCH_BACKGROUND)
+            .size(TAB_SIZE).build();
 
 
 
---
+++ src/main/java/net/archasmiel/thaumcraft/screen/thaumonomicon/lib/Textures.java
@@ -0,0 +1,71 @@
+package net.archasmiel.thaumcraft.screen.thaumonomicon.lib;
+
+import io.github.cottonmc.cotton.gui.widget.data.Texture;
+import net.archasmiel.thaumcraft.Thaumcraft;
+import net.minecraft.util.Identifier;
+
+import static net.archasmiel.thaumcraft.Thaumcraft.MOD_ID;
+import static net.archasmiel.thaumcraft.screen.thaumonomicon.lib.Utility.cutTexture;
+import static net.archasmiel.thaumcraft.screen.thaumonomicon.lib.Utility.loadTexture;
+
+public class Textures {
+
+    private Textures() {
+
+    }
+
+    public static final Integer DEF_COLOR = 0xFFFFFFFF;
+    public static final String RESEARCH_PATH = "textures/other/research/";
+
+
+    // backgrounds for book
+    public static final Texture CONTENTS =
+            new Texture(new Identifier(Thaumcraft.MOD_ID, "textures/gui/thaumonomicon/research.png"));
+    public static final Texture NORMAL_BACKGROUND =
+            new Texture(new Identifier(Thaumcraft.MOD_ID, "textures/gui/thaumonomicon/researchback.png"));
+    public static final Texture ELDRITCH_BACKGROUND =
+            new Texture(new Identifier(Thaumcraft.MOD_ID, "textures/gui/thaumonomicon/researchback_eldritch.png"));
+
+    // research border
+    public static final Texture RESEARCH_BORDER =
+            cutTexture(CONTENTS, 0, 0, 256, 230, 256, 256);
+
+    // tabs
+    public static final Texture RESEARCH_TAB_ACTIVE =
+            cutTexture(CONTENTS, 152, 232, 24, 24, 256, 256);
+    public static final Texture RESEARCH_TAB_INACTIVE =
+            cutTexture(CONTENTS, 176, 232, 24, 24, 256, 256);
+    public static final Texture RESEARCH_TAB_SHADOW =
+            cutTexture(CONTENTS, 200, 232, 24, 24, 256, 256);
+
+    // research forms
+    public static final Texture RESEARCH_SQUARE =
+            cutTexture(CONTENTS, 0, 230, 26, 26, 256, 256);
+    public static final Texture RESEARCH_CIRCLE =
+            cutTexture(CONTENTS, 54, 230, 26, 26, 256, 256);
+    public static final Texture RESEARCH_HEX =
+            cutTexture(CONTENTS, 110, 230, 26, 26, 256, 256);
+    public static final Texture RESEARCH_FLOWER =
+            cutTexture(CONTENTS, 26, 230, 26, 26, 256, 256);
+
+    // weird yellow textures
+    public static final Texture RESEARCH_YELLOW_SQUARE =
+            cutTexture(CONTENTS, 86, 230, 26, 26, 256, 256);
+    public static final Texture RESEARCH_YELLOW_HEX =
+            cutTexture(CONTENTS, 230, 230, 26, 26, 256, 256);
+
+    // Research tabs in original sort
+    public static final Texture RT_BASICS =
+            loadTexture(MOD_ID, RESEARCH_PATH + "tab/r_basics.png");
+    public static final Texture RT_THAUMATURGY =
+            loadTexture(MOD_ID, RESEARCH_PATH + "tab/r_thaumaturgy.png");
+    public static final Texture RT_ALCHEMY =
+            loadTexture(MOD_ID, RESEARCH_PATH + "tab/r_alchemy.png");
+    public static final Texture RT_ARTIFICE =
+            loadTexture(MOD_ID, RESEARCH_PATH + "tab/r_artifice.png");
+    public static final Texture RT_GOLEMANCY =
+            loadTexture(MOD_ID, RESEARCH_PATH + "tab/r_golemancy.png");
+    public static final Texture RT_ELDRITCH =
+            loadTexture(MOD_ID, RESEARCH_PATH + "tab/r_eldritch.png");
+
+}
